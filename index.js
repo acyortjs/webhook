f46232207862d9999dc6db431b64dcaadd1d7c69
LoeiFy <LoeiFy@gmail.com>
2017-04-19 15:51:27 +0800

    works

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyParser = require('koa-bodyparser')
 const crypto = require('crypto')
 const yaml = require('yamljs')
 
-const config = yaml.load('./_config.yml')
+const config = yaml.load('./config.yml')
 
 if (
   !config.secret      ||
@@ -21,34 +21,28 @@ const $header = {
   'Travis-API-Version': 3,
   'Authorization': 'token ' + config.token
 }
-const $body = JSON.stringify({
+const $body = {
   request: {
     branch: config.branch
   }
-})
+}
 const $url = `https://api.travis-ci.org/repo/${encodeURIComponent(config.repository)}/requests`
 
-axios({
-  method: 'POST',
-  url: $url,
-  headers: $header,
-  data: $body
-})
-.then(res => console.log(res))
-
-/*
-
-const hmac = crypto.createHmac('sha1', secret)
 const server = new Koa()
 
 server.use(bodyParser())
 
 server.use(async (ctx, next) => {
   const { header, method, body } = ctx.request
+  const hmac = crypto.createHmac('sha1', config.secret)
   const sign = 'sha1=' + hmac.update(JSON.stringify(body), 'utf-8').digest('hex')
 
   if (method !== 'POST') {
     return ctx.response.status = 403
+  }
+
+  if (header['x-github-event'] === 'ping') {
+    return ctx.response.status = 206
   }
 
   if (header['x-github-event'] !== 'issues') {
@@ -63,10 +57,14 @@ server.use(async (ctx, next) => {
 })
 
 server.use(async (ctx, next) => {
-  ctx.response.type = 'application/json'
-  ctx.response.body = { c: 0 }
+  await axios({
+    method: 'post',
+    url: $url,
+    headers: $header,
+    data: $body
+  })
+  .then(res => ctx.response.status = res.status)
+  .catch(err => ctx.response.status = 500)
 })
 
 server.listen(2333)
-
-*/
